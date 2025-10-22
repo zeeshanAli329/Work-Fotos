4f175d92785b5397adae3de237d443f9ff38e442
Zeeshan Ali <zeeshanali3297624@gmail.com>
2025-10-22 11:27:42 +0500

    card-changes

--- src/app/how-it-works/smarter-section/SmarterSection.jsx
+++ src/app/how-it-works/smarter-section/SmarterSection.jsx
@@ -39,22 +39,6 @@ const SmarterSection = () => {
         <div className="px-[24px]  ">
           {/* ------------cards------------- */}
           <section className="flex flex-col lg:flex-row md:flex-row  justify-center items-center gap-15 md:px-[64px] ">
-            {/* <div className="flex flex-wrap justify-center items-center  font-inter mt-8 lg:mt-10 transition-all duration-300 ease-in-out hover:scale-101 rounded-lg shadow-md ">
-              <div className="md:w-[222px] md:h-[138px]  lg:w-[292px]   w-[210px] h-[110px] py-[16px] px-[12px]   md:px-[29px] md:py-[42px] flex flex-col justify-center items-center gap-5 bg-white rounded-lg ">
-                <div className="flex justify-center items-center gap-2 ">
-                  <img
-                    className="lg:w-[28px] lg:h-[28px] w-[19px] h-[20px] "
-                    src="/how-to-woks-page-file-img.svg"
-                  ></img>
-                  <h1 className="text-[#1E3A8A] font-bold lg:text-[20px] text-[14px] tracking-[0%] leading-[100%]">
-                    Set Up Your Project
-                  </h1>
-                </div>
-                <p className="lg:text-[16px] text-[12px] text-[#000000]  ml-[5px]">
-                  Create a new project in seconds just give it a name and you’re ready to go.
-                </p>
-              </div>
-            </div> */}
             <div className="flex md:hidden lg:hidden ">
               <video
                 src="/video-how-to...mp4"
@@ -68,11 +52,11 @@ const SmarterSection = () => {
 
 
             {card.map((items, index) => (
-              <div className="flex flex-wrap justify-center items-center font-inter mt-8 lg:mt-10 transition-all duration-300 ease-in-out hover:scale-101 rounded-lg shadow-md bg-[#ffffff]"
+              <div className="flex flex-wrap justify-start items-start font-inter mt-8 lg:mt-10 transition-all duration-300 ease-in-out hover:scale-101 rounded-lg shadow-md bg-[#ffffff]"
               key={index}
               >
-                <div className="md:w-[222px] md:h-[138px] lg:w-[292px] lg:h-[187px]  w-[210px] h-[110px] py-[16px] px-[12px]  md:px-[29px] md:py-[42px] flex flex-col justify-start items-start gap-5">
-                  <div className="flex justify-center items-center gap-2 ">
+                <div className="md:w-[222px] md:h-[138px] lg:w-[292px] lg:h-[187px]  w-[210px] h-[140px] py-[16px] px-[12px]  md:px-[29px]  flex flex-col justify-start items-start gap-5">
+                  <div className="flex flex-col justify-start items-start gap-2 ">
                     <img
                     className="lg:w-[28px] lg:h-[28px] w-[19px] h-[20px] "
                     src={items.img}
@@ -81,7 +65,7 @@ const SmarterSection = () => {
                     {items.title}
                   </h1>
                    <p className="lg:text-[16px] text-[12px] text-[#000000] ">
-                  {card.desc}
+                  {items.desc}
                 </p>
                   </div>
                 </div>
